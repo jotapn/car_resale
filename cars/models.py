f115258e2dec739a83f5e3460c14bd5980f31336
Pádua Neto <paduacn@gmail.com>
2024-09-03 22:27:19 -0300

    Criando signals de Post save e post delete

--- cars/models.py
+++ cars/models.py
@@ -14,8 +14,20 @@ class Car(models.Model):
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     plate = models.CharField(max_length=10, blank=True, null=True)
-    value = models.DecimalField(max_digits=10,decimal_places=2,blank=True, null=True)
+    value = models.DecimalField(max_digits=100,decimal_places=2,blank=True, null=True)
     photo = models.ImageField(upload_to='cars',blank=True, null=True)
 
     def __str__(self):
         return self.model
+    
+class CarInventory(models.Model):
+    cars_count = models.IntegerField()
+    cars_values = models.DecimalField(max_digits=100, decimal_places=2,blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering= ['-created_at']
+        #query ordenada por padrao por data de criaçao
+
+    def __str__(self) -> str:
+        return f'{self.cars_count} - {self.cars_values}'
